4b493676b6e94ab12ebd15b1cd907aae55ca1a35
matthoare5 <23655704@edgehill.ac.uk>
2020-02-24 11:12:54 +0000

    Inital Commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+
+// Copy ALL HMTL files
+gulp.task('copyHtml', async function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+})
+
+// Minify JS
+gulp.task('minify', async function() {
+  gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Compile Sass
+gulp.task('sass', async function() {
+  gulp.src('src/scss/*.scss')
+    // If we get any this will show it
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.html', gulp.series('copyHtml'));
+  gulp.watch('src/js/*.js', gulp.series('minify'));
+  gulp.watch('src/scss/*.scss', gulp.series('sass'));
+})
+
+gulp.task('default', gulp.parallel(['copyHtml', 'minify', 'sass', 'watch']));
