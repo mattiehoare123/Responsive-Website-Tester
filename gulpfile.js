9895c5e684f8b3b341662623cb966568dd0f63fd
matthoare5 <23655704@edgehill.ac.uk>
2020-02-24 12:58:58 +0000

    Created variables and done typography

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('minify', async function() {
 
 // Compile Sass
 gulp.task('sass', async function() {
-  gulp.src('src/scss/*.scss')
+  gulp.src('src/scss/**/*.scss')
     // If we get any this will show it
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'))
@@ -26,7 +26,7 @@ gulp.task('sass', async function() {
 gulp.task('watch', function() {
   gulp.watch('src/*.html', gulp.series('copyHtml'));
   gulp.watch('src/js/*.js', gulp.series('minify'));
-  gulp.watch('src/scss/*.scss', gulp.series('sass'));
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
 })
 
-gulp.task('default', gulp.parallel(['copyHtml', 'minify', 'sass', 'watch']));
+gulp.task('default', gulp.series(['copyHtml', 'minify', 'sass', 'watch']));
